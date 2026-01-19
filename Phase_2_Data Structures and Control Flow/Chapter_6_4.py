ed0bf7ab84b47980c0306e77da3597a3c3798557
Arunpandian C <arunpandiancse25@gmail.com>
2026-01-19 20:17:55 +0530

    Update Chapter_6_4.py

--- Phase_2_Data Structures and Control Flow/Chapter_6_4.py
+++ Phase_2_Data Structures and Control Flow/Chapter_6_4.py
@@ -11,7 +11,7 @@ for i in range(1, 6):
 # Number pyramid
 print("\nNumber pyramid:")
 for i in range(1, 6): 
-    for j in range(1, i + 1):
+    for j in range(1, i + 1): 
         print(j, end="")
     print()
 
